c1b8061bcddfa7cddf30c2517692d1a6bc166b34
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2023-11-06 17:01:00 +0330

    refactor

--- src/main/java/ir/hsadeghi/studentmanagementsystem/service/StudentService.java
+++ src/main/java/ir/hsadeghi/studentmanagementsystem/service/StudentService.java
@@ -3,9 +3,14 @@ package ir.hsadeghi.studentmanagementsystem.service;
 import ir.hsadeghi.studentmanagementsystem.entity.Student;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentService {
     List<Student> getAllStudent();
 
-    Student saveStudent(Student student);
+    void saveStudent(Student student);
+
+    Optional<Student> getStudentById(Long id);
+
+    void updateStudent(Student student);
 }
