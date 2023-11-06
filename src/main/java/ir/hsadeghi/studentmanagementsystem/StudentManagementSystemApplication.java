3377096fc5afa2c61f50f3251bcf3083aa786f47
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2023-11-06 15:40:24 +0330

    command line runner to add data to storage

--- src/main/java/ir/hsadeghi/studentmanagementsystem/StudentManagementSystemApplication.java
+++ src/main/java/ir/hsadeghi/studentmanagementsystem/StudentManagementSystemApplication.java
@@ -1,13 +1,30 @@
 package ir.hsadeghi.studentmanagementsystem;
 
+import ir.hsadeghi.studentmanagementsystem.entity.Student;
+import ir.hsadeghi.studentmanagementsystem.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+@RequiredArgsConstructor
+public class StudentManagementSystemApplication implements CommandLineRunner {
+    private final StudentRepository studentRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementSystemApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+//        Student student1 = new Student("Ramesh", "Fadatare", "ramesh@gmail.com");
+//        studentRepository.save(student1);
+//
+//        Student student2 = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
+//        studentRepository.save(student2);
+//
+//        Student student3 = new Student("tony", "stark", "tony@gmail.com");
+//        studentRepository.save(student3);
+    }
 }
