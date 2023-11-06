6c5985bd075804a1992cf5ee97f5a9fb0c2b0b42
Hossein Sadeghi <hossein.sadeghi.official@gmail.com>
2023-11-06 14:06:21 +0330

    add StudentService, StudentServiceImpl

---
+++ src/main/java/ir/hsadeghi/studentmanagementsystem/service/impl/StudentServiceImpl.java
@@ -0,0 +1,8 @@
+package ir.hsadeghi.studentmanagementsystem.service.impl;
+
+import ir.hsadeghi.studentmanagementsystem.service.StudentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+}
